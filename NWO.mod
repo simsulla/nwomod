3bad5fdc691c2e5dd405d3f75b03d8eb3f591056
Simplicissimus.Sulla@gmail.com <Simplicissimus.Sulla@gmail.com@aa2fb6f0-c31c-15a7-0725-5150db06c3d4>
2015-03-08 09:02:33 +0000

    initial from NWO developer r783 2015-03-08 common

---
+++ NWO.mod
@@ -0,0 +1,3 @@
+name = "New World Order" # name of your mod, shown in launcher
+path = "mod/NWO" # location of your mod files
+user_dir = "NwO" # optional. creates a sub directory where settings/map cache/savegames etc can be safely written for your mod and not get mixed up  
---
+++ NWO/common/countries/tt.sh
@@ -0,0 +1,22 @@
+cp Saudi\ Arabia.txt Gulf\ Cooperation\ Council.txt 
+cp Guatemala.txt Central\ American\ Union.txt 
+cp Kenya.txt East\ African\ Federation.txt 
+cp France.txt Europena\ Union.txt 
+cp Guyana.txt CARICOM.txt 
+cp Ethiopia.txt African\ Union.txt 
+cp Nigeria.txt ECOWAS.txt 
+cp Gabon.txt ECCAS.txt 
+cp Bophuthatswana.txt Southern\ African\ Development\ Community.txt 
+cp Uruguay.txt Union\ of\ South\ American\ Nations.txt 
+cp Uruguay.txt Mercosur.txt 
+cp Peru.txt Andean\ Community.txt 
+cp Egypt.txt Arab\ League.txt 
+cp USA.txt NAFTA.txt 
+cp India.txt SAARC.txt 
+cp Australia.txt Pacific\ Islands\ Federation.txt 
+cp Russia.txt Eurasian\ Economic\ Community.txt 
+cp Ottoman\ Empire.txt Turkic\ Council.txt 
+cp Persia.txt Economic\ Cooperation\ Organization.txt 
+cp Morocco.txt Arab\ Maghreb\ Union.txt 
+cp Indonesia.txt ASEAN.txt
+cp Nicaragua.txt Central\ American\ Integration\ System.txt
